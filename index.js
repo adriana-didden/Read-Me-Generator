b25d3c50aaed4922f8917208ad227c48789243db
Adriana <adriana.j.didden@gmail.com>
2020-02-26 10:04:42 -0500

    changed question to message

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
-
+const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require('./Util/generateMarkdown.js')
-
-const api = require('./Util/api.js')
+const api = require('./util/api.js')
 
 const questions =
     [
         {
             type: "input",
             name: "username",
-            question: "what's your GitHub username?",
+            message: "what's your GitHub username?",
         },
         {
             type: "input",
             name: "ContactEmail",
-            question: "what's your email address?",
+            message: "what's your email address?",
         },
 
         {
             type: "input",
             name: "title",
-            question: "what's your project's name?",
+            message: "what's your project's name?",
         },
         {
             type: "input",
             name: "description",
-            question: "please write a short description of your project",
+            message: "please write a short description of your project",
         },
         {
             type: "list",
             name: "license",
             choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-            question: "what kind of license should your project have?",
+            message: "what kind of license should your project have?",
         },
         {
             type: "input",
             name: "install",
-            question: "what command should be run to install dependencies?",
+            message: "what command should be run to install dependencies?",
             default: "npm install"
         },
         {
             type: "input",
             name: "runTest",
-            question: "what command should be run to run tests?",
+            message: "what command should be run to run tests?",
             default: "npm test"
         },
         {
             type: "input",
             name: "usage",
-            question: "what does the user need to know about using the repo?",
+            message: "what does the user need to know about using the repo?",
         },
         {
             type: "input",
             name: "contribute",
-            question: "what does the user need to know about contributing to the repo?"
+            message: "what does the user need to know about contributing to the repo?"
         }
     ]
     
